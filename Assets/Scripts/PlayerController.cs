c873c04f9aaf4c89f2e4b270ee9e39c80b9e5191
Kevin <kzhou10@my.centennialcollege.ca>
2020-08-24 02:29:05 -0400

    Can Shoot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float angle;
 
     public Rigidbody2D rb;
-
+    public Camera cam;
+     
     Vector2 movement;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        var rawmouse = Input.mousePosition;
+        rawmouse.z = 10;
+        mousePos = cam.ScreenToWorldPoint(rawmouse);
     }
 
      void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
---
+++ Assets/bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+
+}
