6d2811c9c56350cbb37e16f1c20bd0c01086c924
Kevin <kzhou10@my.centennialcollege.ca>
2020-08-24 04:55:55 -0400

    Shooting corrected and aim too

---
+++ Assets/Scripts/BlastController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlastController : MonoBehaviour
+{
+
+    public float speed;
+    public float timeToLive;
+
+    Vector3 moveVector;
+
+    private void Start()
+    {
+        moveVector = Vector3.up * speed * Time.fixedDeltaTime;
+        StartCoroutine(DestoryBlast());
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(moveVector); 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator DestoryBlast()
+    {
+        yield return new WaitForSeconds(timeToLive);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,22 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float angle;
 
+    public GameObject blastPrefab; 
+    public Transform FirePoint; 
+
+    public float timebtwshots; //add
+
     public Rigidbody2D rb;
     public Camera cam;
+    private bool canShoot; //add
      
     Vector2 movement;
     Vector2 mousePos;
+
+    void Start() //add
+    {
+        canShoot = true; //add    
+    }
 
     // Update is called once per frame
     void Update()
@@ -22,8 +33,26 @@ public class PlayerController : MonoBehaviour
         var rawmouse = Input.mousePosition;
         rawmouse.z = 10;
         mousePos = cam.ScreenToWorldPoint(rawmouse);
+
+        if (Input.GetButton("Fire1") && canShoot) //add
+        {
+            Shoot();
+        }
+
     }
 
+    private void Shoot()
+    {
+        canShoot = false;
+        Instantiate(blastPrefab, FirePoint.position, transform.rotation);
+        StartCoroutine(ShootCooldown());
+    }
+
+    IEnumerator ShootCooldown()
+    {
+        yield return new WaitForSeconds(timebtwshots);
+        canShoot = true;
+    }
      void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
